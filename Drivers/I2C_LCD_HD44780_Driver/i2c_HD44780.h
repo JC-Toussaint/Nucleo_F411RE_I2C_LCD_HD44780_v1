92cc3f29fc94e36b2d54f4ae6e83186a11fb6d8c
JC-Toussaint <jean-christophe.toussaint@grenoble-inp.fr>
2023-04-27 09:53:37 +0200

    first commit

---
+++ Drivers/I2C_LCD_HD44780_Driver/i2c_HD44780.h
@@ -0,0 +1,13 @@
+#include "stm32f4xx_hal.h"
+
+void lcd_init (void);   // initialize lcd
+//void lcd_send_cmd (char cmd);  // send command to the lcd
+//void lcd_send_data (char data);  // send data to the lcd
+void lcd_disp_on(void);
+void lcd_disp_off(void);
+void lcd_home(void);
+void lcd_clear_display(void);
+void lcd_locate(uint8_t row, uint8_t column);
+void lcd_printchar(unsigned char ascode);
+void lcd_print_string (char *str);  // print string to the lcd
+void lcd_printf(const char *fmt, ...);
